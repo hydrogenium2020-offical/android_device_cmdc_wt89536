9f3914cbf9f8c298270914dbefda4e94d2d1422f
Hydrogenium2020 <107231979+hydrogenium2020-offical@users.noreply.github.com>
2023-04-05 15:57:04 +0800

    Selinux permissive,Add camera,fingerprint,audio firmware

--- device.mk
+++ device.mk
@@ -369,10 +369,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl \
-    android.hardware.vibrator@1.0-service
+    vendor.qti.hardware.vibrator.service
+
+# # Vibrator
+# PRODUCT_PACKAGES += \
+#     android.hardware.vibrator@1.0-impl \
+#     android.hardware.vibrator@1.0-service
 
 # Touch features
 PRODUCT_PACKAGES += \
